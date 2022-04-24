a133b5f94caf47b6252b07428af5ab153993ecc7
Prince Sondakh <102894606+PrinceSondakh@users.noreply.github.com>
2022-04-24 20:00:22 +0800

    Prince

--- src/MainClass.java
+++ src/MainClass.java
@@ -204,7 +204,7 @@ public class MainClass {
         }
     }
 
-    public static void testViewRemoveTodoList(){
+    public static void testViewRemoveTodoList(){ //to test to show viewremovetodolist
         addTodoList("Satu");
         addTodoList("Dua");
         addTodoList("Tiga");
